294ca03b887c4c7fac0e94779533d5e64934806c
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-14 13:54:17 +0300

    addded a picture model

--- insta/models.py
+++ insta/models.py
@@ -33,53 +33,53 @@ class Profile(models.Model):
     def search_user(cls,user):
         return cls.objects.filter(user__username__icontains = user).all()
         
-# class Picture(VoteModel,models.Model):
-#     image = models.ImageField(upload_to = 'images/',blank = False, null = True)
-#     name = models.CharField(max_length = 100)
-#     caption = models.CharField(max_length = 100)
-#     profile = models.ForeignKey(User,on_delete = models.CASCADE)
-#     like_add = models.IntegerField(default=0)
-#     date_uploaded = models.DateTimeField(auto_now_add=True)
-#     # comments = models.CharField(max_length = 1500)
+class Picture(VoteModel,models.Model):
+    image = models.ImageField(upload_to = 'images/',blank = False, null = True)
+    name = models.CharField(max_length = 100)
+    caption = models.CharField(max_length = 100)
+    profile = models.ForeignKey(User,on_delete = models.CASCADE)
+    like_add = models.IntegerField(default=0)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    # comments = models.CharField(max_length = 1500)
     
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
-#     def save_image(self):
-#         self.save()
+    def save_image(self):
+        self.save()
         
-#     def delete_image(self):
-#         self.delete()
+    def delete_image(self):
+        self.delete()
         
-#     @classmethod
-#     def update_caption(self,caption):
-#         updated_cap = cls.objects.filter(id = id).update(caption = caption)
-#         return updated_cap
+    @classmethod
+    def update_caption(self,caption):
+        updated_cap = cls.objects.filter(id = id).update(caption = caption)
+        return updated_cap
     
-#     @classmethod
-#     def get_all_images(cls):
-#         image = cls.objects.all()
-#         return image
+    @classmethod
+    def get_all_images(cls):
+        image = cls.objects.all()
+        return image
     
-#     @classmethod
-#     def get_image_by_id(cls,id):
-#         image = cls.objects.filter(id= id).all()
-#         return image
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id= id).all()
+        return image
     
-#     @classmethod
-#     def search_by_profile(cls,name):
-#         profile = Profile.objects.filter(user__name__icontains = name)
-#         return profile
+    @classmethod
+    def search_by_profile(cls,name):
+        profile = Profile.objects.filter(user__name__icontains = name)
+        return profile
     
-#     @classmethod
-#     def get_one_image(cls,id):
-#         image = cls.objects.get(id = id)
-#         return image
+    @classmethod
+    def get_one_image(cls,id):
+        image = cls.objects.get(id = id)
+        return image
     
     
     
-#     class Meta:
-#         ordering = ['-date_uploaded']
+    class Meta:
+        ordering = ['-date_uploaded']
     
 # class Comments(models.Model):
 #     comment = models.CharField(max_length = 50, blank=True)
