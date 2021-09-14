26c4cc54dddf1f10c4b102f0e836dd5dd96875cc
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-14 14:21:59 +0300

    added the database settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -84,16 +84,16 @@
 # # Database
 # # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'insta',
-#         'USER': 'marynjeri',
-#     'PASSWORD':'njeri2018',
-#     'HOST':'127.0.0.1',
-#     'PORT':'5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'marynjeri',
+    'PASSWORD':'njeri2018',
+    'HOST':'127.0.0.1',
+    'PORT':'5432',
+    }
+}
 # # Password validation
 # # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
