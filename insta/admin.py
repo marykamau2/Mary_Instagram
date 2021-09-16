93964fbdfe4c58ef0e9505ca34c2d418a8c98c79
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-16 19:51:00 +0300

    registered models

--- insta/admin.py
+++ insta/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import  Profile,Picture,Comments
-# # Register your models here.
+from django.contrib import admin
+from .models import  Profile,Picture,Comments
+# Register your models here.
 
-# admin.site.register(Profile)
-# admin.site.register(Picture)
-# admin.site.register(Comments)
+admin.site.register(Profile)
+admin.site.register(Picture)
+admin.site.register(Comments)
